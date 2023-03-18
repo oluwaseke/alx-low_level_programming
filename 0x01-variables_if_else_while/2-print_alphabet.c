b209af12362b97d19264ed9114f0ff33a3cb3518
Oluwaseke Tanimola <ooluwaseke@gmail.com>
2023-03-18 12:20:57 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,14 +4,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 char letter = 'a';
-while (letter <= 'z') 
+while (letter <= 'z')
 {
 putchar(letter);
 letter++;
 }
 putchar('\n');
-return 0;
+return (0);
 }
