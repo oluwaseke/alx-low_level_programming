4de5fa4f71edb388f084ddcbddb71f9b79928e7f
Oluwaseke Tanimola <ooluwaseke@gmail.com>
2023-03-18 12:30:34 +0100

    Fix Issues with Betty

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-    char c = 'z';
-    while (c >= 'a')
-    {
-        putchar(c);
-        c--;
-    }
-    putchar('\n');
-    return (0);
+char c = 'z';
+while (c >= 'a')
+{
+    putchar(c);
+    c--;
+}
+putchar('\n');
+return (0);
 }
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-    int i;
-    for (i = 0; i < 10; i++) 
-    {
-        putchar(i + '0');
-        if (i != 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-    putchar('\n');
-    return 0;
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return 0;
 }
