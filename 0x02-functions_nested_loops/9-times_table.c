992f0e14ec780702396f3f2611930f8f3628725e
Oluwaseke Tanimola <ooluwaseke@gmail.com>
2023-03-23 01:32:48 +0100

    Update 9-timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,36 +4,30 @@
  */
 void times_table(void)
 {
-int i, j, prod, tens, ones;
+int i, j, k;
 
-for (i = 0; i <= 9; i++)
+for (i = 0; i < 10; i++)
 {
-for (j = 0; j <= 9; j++)
+for (j = 0; j < 10; j++)
 {
-prod = i * j;
-tens = prod / 10;
-ones = prod % 10;
-
+k = j * i;
 if (j == 0)
 {
-_putchar('0');
-}
-else if (tens == 0 && j != 0)
-{
-_putchar(' ');
-_putchar(' ');
-}
-else
-{
-_putchar(tens + '0');
+_putchar(k + '0');
 }
 
-_putchar(ones + '0');
-
-if (j != 9)
+if (k < 10 && j != 0)
 {
 _putchar(',');
 _putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
+} else if (k >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
