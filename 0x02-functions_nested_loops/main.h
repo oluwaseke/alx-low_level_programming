9a9c65252643c6f29a5e8a55f08cdc69ef8a2990
Oluwaseke Tanimola <ooluwaseke@gmail.com>
2023-03-24 14:46:51 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,6 +15,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-void print_fibonacci_numbers(void);
-
+void print_fibonacci(void);
 #endif /* MAIN_H */
