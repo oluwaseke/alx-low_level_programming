be0a242e7675db92cfed2ee6e45071b9644b85e2
Oluwaseke Tanimola <ooluwaseke@gmail.com>
2023-03-24 15:13:57 +0100

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,10 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_fibonacci - prints the first 50 numbers
- * in the Fibonacci sequence,
- * starting with 1 and 2,
- * followed by a new line.
  * main - Entry point
  *
  * Return: Always 0 (Success)
